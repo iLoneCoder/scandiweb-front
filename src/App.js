f01515b0bdb5623ffdee20c93c1185a204481555
iLoneCoder <gio.shagidze@gmail.com>
2023-02-20 19:18:45 +0400

    adding HashRouter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import Footer from "./components/layout/Footer";
 import ProductAdd from "./pages/ProductAdd";
 import ProductsList from "./pages/ProductsList";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<ProductsList />} />
         <Route path="/add-product" element={<ProductAdd />} />
       </Routes>
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
